b28d9d9848356e09deb2761567666fb85169e301
ignaciosanabria <ignaciosanabria@outlook.com>
2017-06-01 11:42:55 -0300

    modifique base de datos

--- BaseDeDatos/database.sql
+++ BaseDeDatos/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2017 a las 02:32:33
+-- Tiempo de generación: 01-06-2017 a las 16:42:15
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -30,8 +30,16 @@ CREATE TABLE `auto` (
   `id` int(10) NOT NULL,
   `patente` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
   `marca` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `fecha_ingreso` datetime(6) NOT NULL
+  `color` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `auto`
+--
+
+INSERT INTO `auto` (`id`, `patente`, `marca`, `color`) VALUES
+(1, 'HZQ 995', 'Ford', 'Azul'),
+(2, 'GOA 222', 'Peugeot', 'Negro');
 
 -- --------------------------------------------------------
 
@@ -40,9 +48,22 @@ CREATE TABLE `auto` (
 --
 
 CREATE TABLE `cochera` (
+  `id` int(10) NOT NULL,
   `piso` int(10) NOT NULL,
-  `cocheras reservadas` int(10) NOT NULL
+  `estaLibre` bit(10) NOT NULL,
+  `prioridad` bit(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `cochera`
+--
+
+INSERT INTO `cochera` (`id`, `piso`, `estaLibre`, `prioridad`) VALUES
+(1, 1, b'0000000001', b'0000000000'),
+(2, 1, b'0000000001', b'0000000001'),
+(3, 2, b'0000000001', b'0000000000'),
+(4, 2, b'0000000001', b'0000000001'),
+(5, 3, b'0000000001', b'0000000001');
 
 -- --------------------------------------------------------
 
@@ -52,24 +73,53 @@ CREATE TABLE `cochera` (
 
 CREATE TABLE `empleado` (
   `id` int(10) NOT NULL,
-  `nombre` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
   `legajo` int(10) NOT NULL,
-  `dni` bigint(10) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `apellido` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `fecha_ingreso` datetime(6) NOT NULL
+  `mail` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` varchar(30) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id`, `nombre`, `legajo`, `mail`, `clave`) VALUES
+(1, 'Marcelo', 1, 'empleadouno@gmail.com', '123456'),
+(2, 'Diego', 2, 'empleadodos@yahoo.com', '4567');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `operacion`
+--
+
+CREATE TABLE `operacion` (
+  `id` int(10) NOT NULL,
+  `patente` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_ingreso` datetime(6) NOT NULL,
+  `fecha_salida` datetime(6) NOT NULL,
+  `importe` int(10) NOT NULL,
+  `idCochera` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estacionamiento`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `estacionamiento` (
-  `nombre` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `direccion` int(10) NOT NULL
+CREATE TABLE `usuario` (
+  `id` int(10) NOT NULL,
+  `mail` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `mail`, `clave`) VALUES
+(1, 'administrador@outlook.com', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -85,7 +135,7 @@ ALTER TABLE `auto`
 -- Indices de la tabla `cochera`
 --
 ALTER TABLE `cochera`
-  ADD PRIMARY KEY (`piso`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `empleado`
@@ -93,6 +143,47 @@ ALTER TABLE `cochera`
 ALTER TABLE `empleado`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `operacion`
+--
+ALTER TABLE `operacion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `auto`
+--
+ALTER TABLE `auto`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `cochera`
+--
+ALTER TABLE `cochera`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `operacion`
+--
+ALTER TABLE `operacion`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
